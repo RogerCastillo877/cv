e83de1c6c80f8cc3a2612a1a1c5d58bb87923465
RogerCastillo877 <roger.castillo877@gmail.com>
2025-11-16 16:10:39 -0500

    feat: refactor app component to support multiple timers and enhance alert visibility management

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,17 +4,24 @@ import { FormsModule } from '@angular/forms';
 import { Timer } from './components-class/components/timer/timer';
 import { TimerS } from './components-class/components/timer-s/timer-s';
 import { AlertView } from './components-class/components/alert-view/alert-view';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [FormsModule, Timer, TimerS, AlertView],
+  imports: [FormsModule, Timer, TimerS, AlertView, NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   public isAddTimerVisible: boolean = false;
+  public isEndTimerAlertVisible: boolean = false;
   // public isAddTimerVisibleS = signal(false);
   public time : number = 0;
+  public timers: number[] = [];
+
+  constructor() {
+    this.timers = [10, 20, 30];
+  }
 
   public showAddTimer() {
     this.isAddTimerVisible = true;
@@ -28,5 +35,18 @@ export class AppComponent {
 
   CountdownFinished() {
     console.log("AppComponent: Countdown finished");
+  }
+
+  public showEndTimerAlert() {
+    this.isEndTimerAlertVisible = true;
+  }
+
+  public hideEndTimerAlert() {
+    this.isEndTimerAlertVisible = false;
+  }
+
+  public submitAddTimer() {
+    this.timers.push(this.time);
+    this.hideAddTimer();
   }
 }
